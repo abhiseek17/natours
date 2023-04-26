af330752fa2491063eaecd8aebdd6d9104e6087e
Abhishek Sharma <abhiseek17@gmail.com>
2023-04-26 14:39:03 +0530

    CORS implemented

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
 const enforce = require('express-sslify');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -34,6 +35,8 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1. GLOBAL MIDDLEWARES
+app.use(cors());
+app.options('*', cors());
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 // Set security HTTP Headers
